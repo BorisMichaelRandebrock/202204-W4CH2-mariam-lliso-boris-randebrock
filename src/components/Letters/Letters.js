d3d08bc15c19c40732e71c3bf55d243966816c6b
MariamLliso <contacto.mariamlliso@gmail.com>
2022-04-26 20:17:41 +0200

    Dinamic creation of letters at Letter component

--- src/components/Letters/Letters.js
+++ src/components/Letters/Letters.js
@@ -1,89 +1,44 @@
 import "./Letters.css";
 
-const Letters = () => {
+const Letters = ({ accion }) => {
+  const letters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "Ñ",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
   return (
     <ul className="letters">
-      <li className="letter">
-        <a href="a">A</a>
-      </li>
-      <li className="letter">
-        <a href="b">B</a>
-      </li>
-      <li className="letter">
-        <a href="c">C</a>
-      </li>
-      <li className="letter">
-        <a href="d">D</a>
-      </li>
-      <li className="letter">
-        <a href="e">E</a>
-      </li>
-      <li className="letter">
-        <a href="f">F</a>
-      </li>
-      <li className="letter">
-        <a href="g">G</a>
-      </li>
-      <li className="letter">
-        <a href="h">H</a>
-      </li>
-      <li className="letter">
-        <a href="i">I</a>
-      </li>
-      <li className="letter">
-        <a href="j">J</a>
-      </li>
-      <li className="letter">
-        <a href="k">K</a>
-      </li>
-      <li className="letter">
-        <a href="l">L</a>
-      </li>
-      <li className="letter">
-        <a href="m">M</a>
-      </li>
-      <li className="letter">
-        <a href="n">N</a>
-      </li>
-      <li className="letter">
-        <a href="ñ">Ñ</a>
-      </li>
-      <li className="letter">
-        <a href="o">O</a>
-      </li>
-      <li className="letter">
-        <a href="p">P</a>
-      </li>
-      <li className="letter">
-        <a href="q">Q</a>
-      </li>
-      <li className="letter">
-        <a href="r">R</a>
-      </li>
-      <li className="letter">
-        <a href="s">S</a>
-      </li>
-      <li className="letter">
-        <a href="t">T</a>
-      </li>
-      <li className="letter">
-        <a href="u">U</a>
-      </li>
-      <li className="letter">
-        <a href="v">V</a>
-      </li>
-      <li className="letter">
-        <a href="w">W</a>
-      </li>
-      <li className="letter">
-        <a href="x">X</a>
-      </li>
-      <li className="letter">
-        <a href="y">Y</a>
-      </li>
-      <li className="letter">
-        <a href="z">Z</a>
-      </li>
+      {letters.map((letter) => (
+        <li className="letter" key={letter}>
+          <a href={letter.toLowerCase()} onClick={accion}>
+            {letter}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
